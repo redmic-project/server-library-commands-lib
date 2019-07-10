ac24abf91093e66632fd626de34acb280147bec9
Noel Alonso <noel@redmic.es>
2019-07-10 12:18:45 +0100

    Añade comandos para eventos de usersettings

---
+++ src/main/java/es/redmic/commandslib/usersettings/commands/UpdateSettingsCommand.java
@@ -0,0 +1,41 @@
+package es.redmic.commandslib.usersettings.commands;
+
+/*-
+ * #%L
+ * commands-lib
+ * %%
+ * Copyright (C) 2019 REDMIC Project / Server
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import es.redmic.commandslib.commands.Command;
+import es.redmic.usersettingslib.dto.PersistenceDTO;
+
+public class UpdateSettingsCommand extends Command {
+
+	private PersistenceDTO persistence;
+
+	public UpdateSettingsCommand(PersistenceDTO persistence) {
+		this.persistence = persistence;
+	}
+
+	public PersistenceDTO getPersistence() {
+		return persistence;
+	}
+
+	public void setPersistence(PersistenceDTO persistence) {
+		this.persistence = persistence;
+	}
+}
