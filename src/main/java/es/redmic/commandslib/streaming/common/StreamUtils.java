795c1a67ededdd6d771f1cb4b02bc7b19f31a27f
Noel <noelalonso1986@gmail.com>
2022-01-10 14:18:15 +0000

    Cambia estrategia de consumidor en los streams
    En lugar de usar earliest, se usa latest, para en caso de perder el
    offset, no reprocesar información.

--- src/main/java/es/redmic/commandslib/streaming/common/StreamUtils.java
+++ src/main/java/es/redmic/commandslib/streaming/common/StreamUtils.java
@@ -9,9 +9,9 @@ package es.redmic.commandslib.streaming.common;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -51,7 +51,7 @@ public class StreamUtils {
 		config.put(StreamsConfig.APPLICATION_ID_CONFIG, appId);
 		config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
 		config.put(StreamsConfig.STATE_DIR_CONFIG, stateDir);
-		config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+		config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
 
 		config.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 50);
 		config.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, 300000);
